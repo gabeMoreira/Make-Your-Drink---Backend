0b4c6bd3becb3b56f90d249ea75cc9c7c418a697
Gabriel Moreira <gabriel.moreira999@gmail.com>
2022-12-25 14:48:21 -0300

    adicionando rota de update e autenticação completa

---
+++ src/middlewares/authentication.js
@@ -0,0 +1,21 @@
+import { json } from 'express'
+import jwt from 'jsonwebtoken'
+
+export const authMiddleware = async (req, res, next) => {
+
+    if (req.path === '/login') return next()
+
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(" ")[1]
+
+    if (!token) return res.status(401).json({ message: "Requisição não autorizada." })
+
+    try {
+        jwt.verify(token, process.env.TOKEN_SECRET)
+        return next()
+    } catch (err) {
+        return res.status(401).json({ message: "Requisição não autorizada." })
+    }
+}
+
+export default authMiddleware
---
+++ src/routes/login.js
@@ -0,0 +1,35 @@
+import express from 'express'
+const Router = express.Router()
+import jwt from 'jsonwebtoken'
+import userModel from '../entities/user/userModel.js'
+import bcrypt from 'bcrypt'
+
+Router.route('/').post( async (req, res) => {
+    const { email, password } = req.body
+
+    if (!email || !password) return res.status(401).json({ error: 'Invalid Login' })
+    const findUsers = await userModel.findOne({ email })
+
+    if (!findUsers) {
+        return res.status(401).json({ error: 'Invalid Login' })
+    }
+
+    const passwordCheck = await bcrypt.compare(password, findUsers.password)
+
+    if (!passwordCheck) {
+        return res.status(401).json({ error: 'Invalid Login' })
+    }
+    const token = jwt.sign({
+        id: findUsers.id,
+        email: findUsers.email
+    }, process.env.TOKEN_SECRET)
+
+    return res.status(200).json({
+        message: "Login efetuado com sucesso.",
+        token: token
+    })
+
+
+})
+
+export default Router
