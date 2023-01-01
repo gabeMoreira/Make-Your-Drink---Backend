6d28dd008326d0ce34f71b52f9c1ad4e26648e30
Gabriel Moreira <gabriel.moreira999@gmail.com>
2023-01-01 20:45:42 -0300

    ajustando algumas mensagens de erros no login, e adicionando cookie na resposta da requisição de login efetuado com sucesso

--- src/routes/login.js
+++ src/routes/login.js
@@ -7,22 +7,28 @@ import bcrypt from 'bcrypt'
 Router.route('/').post( async (req, res) => {
     const { email, password } = req.body
 
-    if (!email || !password) return res.status(401).json({ error: 'Invalid Login' })
+    if (!email || !password) return res.status(401).json({ error: 'Preencha os campos corretamente' })
     const findUsers = await userModel.findOne({ email })
 
     if (!findUsers) {
-        return res.status(401).json({ error: 'Invalid Login' })
+        return res.status(401).json({ error: 'E-mail ou senha inválida' })
     }
 
     const passwordCheck = await bcrypt.compare(password, findUsers.password)
 
     if (!passwordCheck) {
-        return res.status(401).json({ error: 'Invalid Login' })
+        return res.status(401).json({ error: 'E-mail ou senha inválida' })
     }
     const token = jwt.sign({
         id: findUsers.id,
         email: findUsers.email
     }, process.env.TOKEN_SECRET)
+
+    res.cookie("jwt", token, {
+        withCredentials: true,
+        httpOnly: false,
+        maxAge: 1000 * 60 * 15, // would expire after 15 minutes
+    })
 
     return res.status(200).json({
         message: "Login efetuado com sucesso.",
