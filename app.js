eb40fb23ffd7ec30971ff54edc24d1d9063c6fe2
Gabriel Moreira <gabriel.moreira999@gmail.com>
2023-01-02 17:39:42 -0300

    ajustando cors, para usar web local

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ dotenv.config()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(middleware)
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}))
 app.get('/', (req, res) => {
     res.status(200).send({ message: 'default route' })
 })
